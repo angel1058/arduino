c5551b0137345ddf6d2d89d4a6ecfa3df5caa27d
angel1058 <malcolm@malcolmsmart.com>
2021-02-01 22:46:31 +0000

    initial commit

--- oilMeter/final/final.ino
+++ oilMeter/final/final.ino
@@ -7,9 +7,10 @@
 #define ECHO 4
 
 RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR int successCount = 0;
 
 int FULL = 1400;
-
+int sleepy = 0;
 const char* ssid = "Phobos"; //Enter SSID
 const char* password = "1058Dr460n!"; //Enter Password
 const char* mqqtUser = "SONOFF"; //Enter SSID
@@ -25,13 +26,7 @@ PubSubClient mqttClient(mqqtBroker , 1883 , callBack , espClient);
 
 void setup() {
 
-  Serial.begin(9600);
-  
-  pinMode(SENSOR , OUTPUT);
-  pinMode(TRIGGER , OUTPUT);
-  pinMode(ECHO , INPUT);
 
-SetupWifi();
  //do the read here...
   takeMeasurement();
 
@@ -40,52 +35,65 @@ SetupWifi();
 void publish(int remaining)
 {
     
-  Serial.println("Connecting to MQTT");
-    if (mqttClient.connect("client" , mqqtUser , password))
+  Serial.println("Connecting to MQTT attempt "  + String(bootCount) );
+    if (!mqttClient.connect("client" , mqqtUser , password))
+      Serial.println("failed");
+      else
+      {
   Serial.println("connected");
-  else
-    Serial.println("failed");
-    char val[8];
+//  else
+    char val[8];char val1[8];
 
-itoa(remaining, val , 10);
-
+//itoa(remaining, val , 10);
+itoa(++successCount, val , 10);
+Serial.println("Success Count : " + String(successCount) + " - BootCount " + String(bootCount));
 mqttClient.publish("esp/oil/liters" , val);
-Serial.println("published");
-//client.flush();
-Serial.println("flushed");
-mqttClient.disconnect();
-Serial.println("disconnected");
 
+itoa(sleepy, val1 , 10);
+mqttClient.publish("esp/oil/sleep" , val1);
+
+itoa(bootCount, val , 10);
+mqttClient.publish("esp/oil/bootCount" , val);
+
+itoa(bootCount - successCount, val , 10);
+mqttClient.publish("esp/oil/failCount" , val);
+
+//Serial.println("published");
+mqttClient.flush();
+//Serial.println("flushed");
+mqttClient.disconnect();
+//Serial.println("disconnected");
+      }
 }
 
 
 void SetupWifi()
 {
-  Serial.println("Connecting..");
+  //Serial.println("Connecting..");
     WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
-    delay(500);
+    delay(100);
     Serial.print(".");
   }
 
-  Serial.println("");
+//  Serial.println("");
   Serial.println("WiFi connection Successful");
 
 }
 
 void powerUpSensor()
 {
-  Serial.println("Powering up sensor");
+//  Serial.println("Powering up sensor");
   digitalWrite(SENSOR , HIGH);
-  delay(1000);
+  delay(100);
 }
 
 void powerDownSensor()
 {
-  Serial.println("Powering down sensor");
+//  Serial.println("Powering down sensor");
   digitalWrite(SENSOR , LOW);
-  delay(1000);
+  delay(100);
 }
 
 int readFromSensor()
@@ -98,7 +106,7 @@ int readFromSensor()
   long duration = pulseIn(ECHO, HIGH );
   //Calculate the dwqwistance:
   int distance = duration * 0.034 / 2;
-  Serial.println(distance);
+//  Serial.println(distance);
 
   return distance;
 }
@@ -107,28 +115,47 @@ int calculateLitresRemaining(int level)
 {
   int used = ( level * 61 * 185 ) / 1000;
   int remainingLitres = FULL - used;
-  Serial.println("Remaining liters : " + String(remainingLitres) );
+  //Serial.println("Remaining liters : " + String(remainingLitres) );
   return remainingLitres;
 }
 
 void takeMeasurement()
 {
+    Serial.begin(115200);
+    Serial.print("!");
+  
+    pinMode(SENSOR , OUTPUT);
+    pinMode(TRIGGER , OUTPUT);
+    pinMode(ECHO , INPUT);
+
+    SetupWifi();
     powerUpSensor();
     int distance = readFromSensor();
     int litresRemaining = calculateLitresRemaining(distance);
     powerDownSensor();
     publish(litresRemaining);
+    Serial.println("#");
 }
 
 #define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  10800        /* Time ESP32 will go to sleep (in seconds) */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
 
 
 void loop() {
 
 ++bootCount;
-esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * 1000000ULL);
-  Serial.println("Going to sleep for " + String(TIME_TO_SLEEP) +" seconds : iteration : " + String(bootCount));
+sleepy = TIME_TO_SLEEP;
+if ( bootCount > 10)
+  sleepy *= 10;
+else if ( bootCount > 100)
+  sleepy *= 100;
+ else if ( bootCount > 1000)
+   sleepy *= 1000;
+   
+  
+ 
+esp_sleep_enable_timer_wakeup(sleepy * 1000000ULL);
+  Serial.println("Going to sleep for " + String(sleepy) +" seconds : iteration : " + String(bootCount));
 
   //Go to sleep now
   esp_deep_sleep_start();
